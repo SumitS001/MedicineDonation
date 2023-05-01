14a6ee1b4297d44802a836f782c872f696d93496
Sumit Singh <59934187+SumitS001@users.noreply.github.com>
2023-05-01 23:54:20 +0530

    Update settings.py

--- donate/settings.py
+++ donate/settings.py
@@ -118,12 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = [
-    os.path.join(BASE_DIR, "static") 
-]
+STATIC_URL = '/static/'
 
 
 # Added manually
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static") 
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
