f91edff84e9c428c0687f1c0a2eb9892d74a2b7d
Sumit Singh <sumit.singhss194@gmail.com>
2023-05-02 00:24:34 +0530

    change

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "build start"
 
-python3.9 -m pip install requirements.txt
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 
 echo "build ended"
